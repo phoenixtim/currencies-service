9f5a3c9f563f0d40f7f22df5d044fb83c4183464
Artem Apartsev <phoenixtim@yandex.ru>
2021-08-20 01:19:42 +0700

    add exchange rates repository

---
+++ src/repositories/base.js
@@ -0,0 +1,65 @@
+/** @typedef {import('mongoose').Document} DocumentType */
+
+/**
+ * @typedef {{
+ *  id: string,
+ * }} BaseRepositoryItem
+ */
+/**
+ * @typedef {{
+ *  id?: string,
+ * }} BaseRepositoryValues
+ */
+
+class BaseRepository {
+  /**
+   * @protected
+   * @param {object} item
+   * @returns {BaseRepositoryItem}
+   */
+  static format(item) {
+    return item
+  }
+}
+module.exports.BaseRepository = BaseRepository
+
+/**
+ * @typedef {{
+ *  createdAt?: Date,
+ *  updatedAt?: Date,
+ * }} MongoRepositoryItemSpecificValues
+ */
+/**
+ * @typedef {(
+ *  BaseRepositoryValues & MongoRepositoryItemSpecificValues
+ * )} MongoRepositoryItemValues
+ */
+/** @typedef {( BaseRepositoryItem & MongoRepositoryItemSpecificValues )} MongoRepositoryItem */
+
+class MongoRepository extends BaseRepository {
+  // TODO: перенести общие методы из ExchangeRatesRepository сюда
+
+  /**
+   * @protected
+   * @param {DocumentType} item
+   * @returns {MongoRepositoryItem}
+   */
+  static format(item) {
+    if (!item) return null
+
+    const itemData = item.toJSON ?
+      item.toJSON() :
+      item
+    const formattedItem = {
+      ...itemData,
+      id: String(itemData._id),
+    }
+
+    delete formattedItem._id
+    // eslint-disable-next-line no-underscore-dangle
+    delete formattedItem.__v
+
+    return formattedItem
+  }
+}
+module.exports.MongoRepository = MongoRepository
---
+++ src/repositories/exchangeRates.js
@@ -0,0 +1,136 @@
+/** @typedef {import('mongoose').Document} DocumentType */
+/** @typedef {import('mongoose').Types.ObjectId} ObjectIdType */
+
+/** @typedef {import('./base').MongoRepositoryItem} MongoRepositoryItem */
+/** @typedef {import('./base').MongoRepositoryItemValues} MongoRepositoryItemValues */
+
+const mongoose = require('mongoose')
+
+const { MongoRepository } = require('./base')
+
+const { Schema } = mongoose
+
+/**
+ * @typedef {{
+ *  bank?: string,
+ *  currencyBase?: string,
+ *  currencyTarget?: string,
+ *  price?: {
+ *    buy?: number,
+ *    sell?: number,
+ *  },
+ * }} ExchangeRateSpecificValues
+ */
+/** @typedef {(MongoRepositoryItemValues & ExchangeRateSpecificValues)} ExchangeRateValues */
+/** @typedef {(DocumentType & ExchangeRateSpecificValues)} ExchangeRateDocument */
+/** @typedef {(MongoRepositoryItem & ExchangeRateSpecificValues)} ExchangeRateType */
+
+const exchangeRateSchema = new Schema({
+  bank: {
+    type: String,
+    index: true,
+    required: true,
+  },
+  currencyBase: {
+    type: String,
+    index: true,
+    required: true,
+  },
+  currencyTarget: {
+    type: String,
+    index: true,
+    required: true,
+  },
+  price: {
+    buy: Number,
+    sell: Number,
+  },
+}, {
+  timestamps: true,
+})
+
+const ExchangeRate = mongoose.model('ExchangeRate', exchangeRateSchema)
+
+class ExchangeRatesRepository extends MongoRepository {
+  /**
+   * @param {ExchangeRateValues} values
+   * @returns {Promise<ExchangeRateType>}
+   */
+  static async upsert({ bank, currencyBase, currencyTarget, price }) {
+    const exchangeRate = await ExchangeRate.findOneAndUpdate({
+      bank,
+      currencyBase,
+      currencyTarget,
+    }, {
+      price,
+    }, { upsert: true, new: true, lean: true })
+
+    return ExchangeRatesRepository.format(exchangeRate)
+  }
+
+  /**
+   * @param {ExchangeRateValues} filter
+   * @returns {Promise<ExchangeRateType>}
+   */
+  static async findOne(filter) {
+    const exchangeRate = await ExchangeRate.findOne(ExchangeRatesRepository.formatFilter(filter))
+
+    return ExchangeRatesRepository.format(exchangeRate)
+  }
+
+  /**
+   * @param {object} params
+   * @param {ExchangeRateValues} params.filter
+   * @param {number} params.limit
+   * @param {number} params.offset
+   * @returns {Promise<ExchangeRateType[]>}
+   */
+  static async find({ filter, limit, offset }) {
+    const exchangeRates = await ExchangeRate.find(
+      ExchangeRatesRepository.formatFilter(filter),
+      undefined,
+      {
+        limit,
+        skip: offset,
+        sort: { bank: 1, currencyBase: 1, currencyTarget: 1 },
+      },
+    )
+
+    return exchangeRates.map(ExchangeRatesRepository.format)
+  }
+
+  /**
+   * @param {ExchangeRateValues} filter
+   * @returns {Promise<number>}
+   */
+  static async count(filter) {
+    const exchangeRatesNumber = await ExchangeRate.countDocuments(
+      ExchangeRatesRepository.formatFilter(filter),
+    )
+
+    return exchangeRatesNumber
+  }
+
+  /**
+   * @protected
+   * @param {ExchangeRateDocument} item
+   * @returns {ExchangeRateType}
+   */
+  static format(item) {
+    return super.format(item)
+  }
+
+  /**
+   * @protected
+   * @param {ExchangeRateValues} filter
+   */
+  static formatFilter({ bank, currencyBase, currencyTarget }) {
+    const filter = {}
+    if (bank) filter.bank = bank
+    if (currencyBase) filter.$or = [{ currencyBase }]
+    if (currencyTarget) filter.$or = [...(filter.$or || []), { currencyTarget }]
+
+    return filter
+  }
+}
+module.exports.ExchangeRatesRepository = ExchangeRatesRepository
