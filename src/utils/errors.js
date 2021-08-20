3a5543412daf8e21568a4fff135996b2e9b0eade
Artem Apartsev <phoenixtim@yandex.ru>
2021-08-21 02:27:23 +0700

    update error classes

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,3 +1,28 @@
 /** Basic Error class for exceptions in service */
-class CurrencyServiceError extends Error {}
+class CurrencyServiceError extends Error {
+  /**
+   * @param {{
+   *  message: string,
+   *  httpCode?: number,
+   * }} params
+   */
+  constructor({ message, httpCode = 500 }) {
+    super(message)
+
+    this.httpCode = httpCode
+  }
+}
 module.exports.CurrencyServiceError = CurrencyServiceError
+
+class BadRequestError extends CurrencyServiceError {
+  /**
+   * @param {{
+   *  message?: string,
+   *  httpCode?: number,
+   * }} params
+   */
+  constructor({ message = 'Bad request', httpCode = 400 } = {}) {
+    super({ message, httpCode })
+  }
+}
+module.exports.BadRequestError = BadRequestError
